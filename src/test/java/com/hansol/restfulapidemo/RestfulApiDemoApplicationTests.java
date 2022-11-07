9ebc3d735bd91811c88186cd205f92d37788dd7e
hansol9 <hansol9@gmail.com>
2022-11-07 15:12:16 -0500

    project creation

---
+++ src/test/java/com/hansol/restfulapidemo/RestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.restfulapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
